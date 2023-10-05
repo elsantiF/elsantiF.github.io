17b2e037854b0624f4caaa609db5e5949a4e1456
elsantiF <santiagofarall@gmail.com>
2023-10-05 14:22:29 -0300

    Added new projects

--- src/routes/sections/Projects.svelte
+++ src/routes/sections/Projects.svelte
@@ -32,16 +32,18 @@
         Actually I only have one public project in my GitHub account, mostly beacause I never uploaded, finished,
         have sensible date or are in my old GitLab account, or simply I don't like the final state of it. 
         Anyways, this is my (actual) list of public projects:
+        Anyways, this is my (actual) list of public projects:
     </h3>
 
-    <div class="project">
-        <h3 class="project_title">KOPMod - <a href="https://github.com/elsantiF/KOPMod">Link</a></h3>
-        <h3 class="project_desc">
-            As a rocket fan, when Kerbal Space Program 2 was launched I immediately buy it, but the game had a poor performance.
-            So I wrote this little mod to slightly increase the performance, and surprisingly it worked, some people passed from 8 FPS to 20.
-            As new patches are released to the game, the performance is getting better, so I suspend the development of the mod for a while.
-            I made the mod in 48 hours after the release of the game, so isn't well programmed, but works.
-        </h3>
+    <div id="projects">
+    {#each projects as project}
+        <div class="project">
+            <h3 class="project_title"><a href={project.link}>{project.title}</a></h3>
+            <h3 class="project_desc">
+                {project.desc}
+            </h3>
+        </div>
+    {/each}
     </div>
 </article>
 
@@ -50,6 +52,16 @@
     @import '../../styles/sizes.scss';
 
     article {
+        @include front-in-black;
+
+        #projects {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+
+            @media screen and (max-width: 1080px) {
+                grid-template-columns: 1fr;
+        }
+    }
         @include front-in-black;
 
         #projects {
@@ -78,6 +90,7 @@
 
         .project_desc {
             margin: 0 1em;
+            width: 90%;
             width: 90%;
             max-width: 100%;
         }
